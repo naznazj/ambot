5082e5526cfceee6918b2262c1e7a1694033a6b5
Joe Michael <joemichaeltanilon@gmail.com>
2024-08-08 20:07:39 +0800

    hey

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/kapoy/",
   plugins: [react()],
-  base: "/ambot/"
+  build: {
+    outDir: 'dist',
+  },
 })
